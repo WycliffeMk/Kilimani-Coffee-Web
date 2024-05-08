aab7922846bef91bbce6283706066a09b526f3bf
+ <omuhindaaaron@gmail.com>
2024-05-09 00:03:19 +0300

    refactor: make changes on About and App.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-import img from "../assets/img/about.jpg";
 import { Button } from "react-scroll";
 
 const About = () => {
   return (
-    <div>
-      <h1>About Us</h1>
+    <div className=" min-h-screen flex flex-col items-center justify-center lg:px-32 px-5 bg-backgroundColor">
+      <h1 className=" font-semibold text-center text-4xl lg:mt-14 mt-24 mb-8">
+        About Us
+      </h1>
 
-      <div>
-        <div>
-          <img src={img} alt="img" />
+      <div className=" flex flex-col lg:flex-row items-center gap-5">
+        <div className=" w-full lg:w-2/4">
+          <img
+            className=" rounded-lg"
+            src="src/assets/img/about.jpg"
+            alt="img"
+          />
         </div>
-        <div>
-          <h2>What Makes Our Coffee Unique?</h2>
+        <div className=" w-full lg:w-2/4 p-4 space-y-3">
+          <h2 className=" font-semibold text-3xl">
+            What Makes Our Coffee Unique?
+          </h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
             repellat?
