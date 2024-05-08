57ba3dc24eb6c99d7486a4ee86a14882645ce3d4
Wycliffe Munyes <wyclefmunyes@gmail.com>
2024-05-08 23:03:05 +0300

    feat:complete home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import Navbar from "./components/Navbar"
+import Home from "./components/Home"
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <main>
+        <div>
+          <Home/>
+        </div>
+      </main>
     </div>
   )
 }
