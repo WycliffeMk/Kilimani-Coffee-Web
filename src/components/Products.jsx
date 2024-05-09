ba6b32c47deaf2554e106ed34a49be0ceb5d522b
ayub <ayoubhassanyassin@gmail.com>
2024-05-09 16:53:36 +0300

    products

---
+++ src/components/Products.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ProductCard from "../layouts/ProductCard";
+
+const Product = () => {
+  return (
+    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 bg-backgroundColor">
+      <h1 className=" font-semibold text-center text-4xl lg:mt-14 mt-24 mb-8">
+        Our Products
+      </h1>
+
+      <div className=" flex flex-col lg:flex-row gap-12 justify-center">
+        <ProductCard img="public/images/Nespresso.jpeg" title="Nespresso" />
+        <ProductCard img="public/images/AeroPress.jpeg" title="AeroPress" />
+        <ProductCard img="public/images/Chamax.jpeg" title="Chemex" />
+      </div>
+    </div>
+  );
+};
+
+export default Product;
